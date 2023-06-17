33a64c6c1facb0269716fd461dfe408e54bd673d
“Mateus <devmateussnr@gmail.com>
2023-06-17 15:32:07 -0300

    Init push

---
+++ Case2/Case2/Entities/Registration.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Case2.Entities
+{
+    class Registration
+    {
+        public int RegistrationNumber { get; set; }
+
+        public override int GetHashCode()
+        {
+            return RegistrationNumber.GetHashCode();
+            //O comparador será baseado no hashcode da matrícula
+            //Critério de igualdade é a matricula
+        }
+
+        public override bool Equals(object obj)//Qnd uma matricula vai ser igual a outro obj ?
+        {
+            if (!(obj is Registration))//Testa se o obj realmente é do tipo matricula
+            {
+                return false;//objs não são iguais
+            }
+            Registration other = obj as Registration;//downcasting
+            return RegistrationNumber.Equals(other.RegistrationNumber);//Uma matricula é igual a outra
+            //quando o registrationnumber deles forem iguais
+        }
+
+        //Uma matrícula repetida, não é adicionada ao conjunto pois o equals e o hashcode são baseados
+        //no número de matrícula.
+    }
+}
---
+++ Case2/Case2/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Case2.Entities;
+
+namespace main
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HashSet<Registration> registrations = new HashSet<Registration>();
+
+            Console.Write("How many students for course A: ");
+            int a = int.Parse(Console.ReadLine());
+
+            for(int i = 0; i < a; i++)
+            {
+                int registration = int.Parse(Console.ReadLine());
+                registrations.Add(new Registration { RegistrationNumber = registration });
+            }
+
+            Console.Write("How many students for course B: ");
+            int b = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < b; i++)
+            {
+                int registration = int.Parse(Console.ReadLine());
+                registrations.Add(new Registration { RegistrationNumber = registration });
+            }
+
+            Console.Write("How many students for course C: ");
+            int c = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < c; i++)
+            {
+                int registration = int.Parse(Console.ReadLine());
+                registrations.Add(new Registration { RegistrationNumber = registration });
+            }
+
+            Console.WriteLine("Total students: " + registrations.Count);
+        }
+    }
+}
